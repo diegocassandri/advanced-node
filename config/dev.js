1ee4b22a850f169ac22342103a25bea4579bbf38
Leonardo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-06-05 13:43:01 -0300

    initial commit

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://leonardo:1password@ds255889.mlab.com:55889/blog_dev',
   cookieKey: '123123123'
 };
