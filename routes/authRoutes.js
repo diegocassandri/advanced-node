e170364aebfb9559bf95f4927da0078e408f8ef7
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-06-06 01:55:28 -0300

    lecture 51 finished

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,6 @@ module.exports = app => {
     '/auth/google/callback',
     passport.authenticate('google'),
     (req, res) => {
-      console.log('Callback accessed');
       res.redirect('/blogs');
     }
   );
