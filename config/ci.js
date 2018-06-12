79ee0469696ff6386ef072f600b65c347716fab9
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-06-12 00:08:59 -0300

    travis ci configured

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "773485991268-1r0likl3439la7087tq0qu6hnambh7qv.apps.googleusercontent.com",
   googleClientSecret: "w5VYiQX2iVzYtC_e9xnC3X5i",
-  mongoURI: 'mongodb://leonardo:1password@ds255889.mlab.com:55889/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisURL: 'redis://127.0.0.1:6379'
 };
