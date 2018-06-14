747a681d1959479a396b5be6f6736cf0b5b459c2
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-06-13 21:06:50 -0300

    added upload capabilities

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,9 @@ module.exports = {
   googleClientID:
     "773485991268-1r0likl3439la7087tq0qu6hnambh7qv.apps.googleusercontent.com",
   googleClientSecret: "w5VYiQX2iVzYtC_e9xnC3X5i",
-  mongoURI: 'mongodb://leonardo:1password@ds255889.mlab.com:55889/blog_dev',
-  cookieKey: '123123123',
-  redisURL: 'redis://127.0.0.1:6379'
+  mongoURI: "mongodb://leonardo:1password@ds255889.mlab.com:55889/blog_dev",
+  cookieKey: "123123123",
+  redisURL: "redis://127.0.0.1:6379",
+  accessKeyId: "AKIAJKEZIMVVM4N5QJGA",
+  secretAccessKey: "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"
 };
